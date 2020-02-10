2e189c510420a7e0df77ba3d22babb90a0a40d66
Himanshu <himanshu.sahu@nutanix.com>
2020-02-10 12:13:44 +0530

    first time code

---
+++ app/src/main/java/com/nh/boom/FirebaseInstanceIdService.java
@@ -0,0 +1,28 @@
+package com.nh.boom;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseInstanceIdService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        Log.e("NEW_TOKEN", s);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.i("firebase", "data :- " + remoteMessage.getData().toString());
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i("firebase", "created");
+    }
+}
